6ce1729e6974060a0509c210109c4b3b3c60c232
muniprasad_laptop <muniprasad.chata@mizpahsoft.com>
2018-09-03 16:42:48 +0530

    3rd

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -11,6 +11,7 @@ export class EmployeeComponent {
     LastNamme: string= 'Muni';
     salary:number =10000;
     department: string ='software';
+    imgPath: string ="https://angular.io/assets/images/logos/angular/logo-nav@2x.png";
     empName(): string{
         return this.firstname + ' ' + this.LastNamme;
     }
